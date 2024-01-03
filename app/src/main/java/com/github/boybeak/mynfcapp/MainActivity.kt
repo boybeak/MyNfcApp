80d64442812071ec90d573a1c8185af2b7cc494f
gaoyunfei <gaoyunfei@finogeeks.club>
2024-01-03 18:00:57 +0800

    add nfca

--- app/src/main/java/com/github/boybeak/mynfcapp/MainActivity.kt
+++ app/src/main/java/com/github/boybeak/mynfcapp/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.github.boybeak.mynfcapp
 
-import android.app.Activity
 import android.app.PendingIntent
-import android.content.Context
-import android.content.ContextWrapper
 import android.content.Intent
 import android.nfc.NdefMessage
 import android.nfc.NdefRecord
 import android.nfc.NfcAdapter
 import android.nfc.Tag
 import android.nfc.tech.Ndef
+import android.nfc.tech.NfcA
+import android.nfc.tech.TagTechnology
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -25,16 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import com.github.boybeak.mynfcapp.ui.theme.MyNfcAppTheme
 
-class MainActivity : ComponentActivity() {
+private const val TAG = "MainActivity"
 
-    companion object {
-        private const val TAG = "MainActivity"
-    }
+class MainActivity : ComponentActivity() {
 
     private val nfcAdapter by lazy {
         NfcAdapter.getDefaultAdapter(this)
@@ -48,7 +42,7 @@ class MainActivity : ComponentActivity() {
         PendingIntent.getActivity(this, 0, nfcIntent, 0)
     }
 
-    private var ndefTech: Ndef? = null
+    private var tagTechState = mutableStateOf<TagTechnology?>(null)
 
     private val techListState = mutableStateOf("")
 
@@ -61,13 +55,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainPanel(techListState.value, writeClick = {
-                        ndefTech?.run {
-                            val rcd = NdefRecord.createUri("")
-                            val ndefMsg = NdefMessage(rcd)
-                            writeNdefMessage(ndefMsg)
-                        }
-                    })
+                    MainPanel(tagTechState.value, techListState.value)
                 }
             }
         }
@@ -80,6 +68,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onPause() {
         super.onPause()
+        nfcAdapter.enableReaderMode(this, { TODO("Not yet implemented") }, 0, Bundle())
         nfcAdapter.disableForegroundDispatch(this)
     }
 
@@ -90,22 +79,80 @@ class MainActivity : ComponentActivity() {
             NfcAdapter.ACTION_NDEF_DISCOVERED -> {
                 val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG) ?: return
                 techListState.value = tag.techList.joinToString()
-                if (tag.techList.contains(Ndef::class.java.name)) {
-                    ndefTech = Ndef.get(tag)
+                if (tag.techList.contains(NfcA::class.java.name)) {
+                    tagTechState.value = NfcA.get(tag)
+                } else if (tag.techList.contains(Ndef::class.java.name)) {
+                    tagTechState.value = Ndef.get(tag)
                 }
-                Log.d(TAG, "onNewIntent ")
             }
         }
     }
+}
 
+@OptIn(ExperimentalStdlibApi::class)
+fun selectMainDir(nfcA: NfcA) {
+//    val response = transceive(nfcA)
+//    Log.d(TAG, "selectMainDir response(${response.size})=[${response.joinToString { it.toHexString() }}]")
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+fun writeNfcA(nfcA: NfcA) {
+    val response = transceive(nfcA, byteArrayOf(0xA2.toByte(), 0x02, 0x00, 0x00, 0xFF.toByte(), 0xFF.toByte()))
+    Log.d(TAG, "writeNfcA response(${response.size})=[${response.joinToString { it.toHexString() }}]")
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+fun readNfcA(nfcA: NfcA) {
+    val response = transceive(nfcA, byteArrayOf(0x30, 0x10))
+    Log.d(TAG, "readNfcA response(${response.size})=[${response.joinToString { it.toHexString() }}]")
+}
+
+private fun transceive(nfcA: NfcA, data: ByteArray): ByteArray {
+    nfcA.connect()
+    nfcA.timeout = 2000
+    val response = nfcA.transceive(data)
+    nfcA.close()
+    return response
 }
 
 @Composable
-fun MainPanel(techList: String, writeClick: () -> Unit) {
+fun MainPanel(tech: TagTechnology?, techList: String) {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = techList, textAlign = TextAlign.Center)
-        Button(onClick = writeClick) {
-            Text(text = "writeMessage")
+
+        when(tech) {
+            is NfcA -> {
+                val nfcA = tech as NfcA
+                Button(onClick = {
+                    selectMainDir(nfcA)
+                }) {
+                    Text(text = "selectMainDir")
+                }
+                Button(onClick = {
+                    writeNfcA(nfcA)
+                }) {
+                    Text(text = "write")
+                }
+                Button(onClick = {
+                    readNfcA(nfcA)
+                }) {
+                    Text(text = "read")
+                }
+            }
+            is Ndef -> {
+                Button(onClick = {
+                    val ndef = tech as Ndef
+                    ndef.run {
+                        connect()
+                        val rcd = NdefRecord.createUri("https://www.youtube.com")
+                        val ndefMsg = NdefMessage(rcd)
+                        writeNdefMessage(ndefMsg)
+                        close()
+                    }
+                }) {
+                    Text(text = "writeMessage")
+                }
+            }
         }
     }
 }
